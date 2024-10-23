bff3415609375d524fe320ec4640d5b1785e634d
Anna-0301 <annabogd123@gmail.com>
2024-10-23 10:40:41 +0500

    Слово заканчивается на ... [#505236 #518510] - правка 1

--- src/main/java/ru/job4j/array/EndsWith.java
+++ src/main/java/ru/job4j/array/EndsWith.java
@@ -4,9 +4,9 @@ public class EndsWith {
     public static boolean endsWith(char[] word, char[] postfix) {
         boolean result = true;
         for (int i = 0; i < postfix.length; i++) {
-            result = word[word.length - i - 1] == postfix[postfix.length - i - 1];
-            if (!result) {
-                break;
+            if (word[word.length - i - 1] != postfix[postfix.length - i - 1]) {
+            result = false;
+            break;
             }
         }
         return result;
